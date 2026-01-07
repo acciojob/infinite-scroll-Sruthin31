1f639a566a0113fc89f4c625e90ccd0959983596
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-01-07 11:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
+const list = document.getElementById("infi-list");
+let item = 1;
 
+funtion additem(count){
+	for(let i =0;i<count;i++){
+		const li = document.createElement("li");
+		li.textContent = `item ${item++}`;
+		li.appendChild(li);
+	}
+}
+additem(10);
+
+list.EventListener("scroll",()=>{
+	if(list.scrollTop+list.clientHeight>=list.scrollHeight-1){
+
+		additem(2);
+	}
+})
