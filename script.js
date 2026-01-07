544bdf9700d08819efcae49006951d1067e980d3
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-01-07 11:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ let item = 1;
 funtion additem(count){
 	for(let i =0;i<count;i++){
 		const li = document.createElement("li");
-		li.textContent = `item ${item++}`;
+		li.textContent = `Item ${item}`;
 		li.appendChild(li);
+		item++;
 	}
 }
 additem(10);
 
-list.EventListener("scroll",()=>{
-	if(list.scrollTop+list.clientHeight>=list.scrollHeight-1){
+list.addEventListener("scroll",()=>{
+	if(list.scrollTop+list.clientHeight>=list.scrollHeight){
 
 		additem(2);
 	}
